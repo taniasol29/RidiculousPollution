bb404acb431c33dbde8361ea98c8deb64879cc98
Tatiana Soldatova <tsoldatova@yahoo.fr>
2022-07-04 19:59:55 -0400

    peu de changements

--- Assets/Scripts/DestroyCollectible.cs
+++ Assets/Scripts/DestroyCollectible.cs
@@ -18,6 +18,9 @@ public class DestroyCollectible : MonoBehaviour
 
     [SerializeField] ParticleSystem explosion;
 
+    //values
+    public int goValue;
+    public bool isDestroyed = false;
 
     private void Awake()
     {
@@ -30,9 +33,6 @@ public class DestroyCollectible : MonoBehaviour
         leftClick = aMap.FindAction("LeftClick");
         leftClick.started += OnLeftClick;
     }
-
-    
-
 
     private void OnEnable()
     {
